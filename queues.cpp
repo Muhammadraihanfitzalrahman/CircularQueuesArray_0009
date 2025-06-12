b097bba2906a481655cbcc0e10625672aa7042fe
muhammadraihanfitzalrahman <Ayamtulebo19@gmail.com>
2025-06-12 10:24:39 +0700

    menambahkan class queues

--- queues.cpp
+++ queues.cpp
@@ -1,4 +1,46 @@
 #include <iostream>
 using namespace std;
 
-cl
+class Queues {
+    int FRONT, REAR, max = 5;
+    int queues_array[5]
+
+public:
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert() {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        //Cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max  -1) || (FRONT == REAR + 1)) {
+            cout << "``\n Queues overflow\n";
+            return;
+        }
+
+        //Cek apakah antrian kosong
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else{
+            //Jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR ==max -1)
+               REAR =0;
+            else
+               REAR = REAR + 1;
+        }
+        queues_array[REAR] = num;
+    }
+    void remove() {
+        //Cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queues underflow\n";
+        }
+    }
+}
