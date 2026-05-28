16c889236ff4d0ba3fa156e3bf967f49581c8b8f
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-05-28 17:56:13 +0700

    Digunakan untuk menambahkan data ke queue.

--- QUEUE.cpp
+++ QUEUE.cpp
@@ -14,4 +14,65 @@ public:
         FRONT = -1;
         REAR = -1;
     }
-}
+
+    void insert()
+    {
+        int num;
+
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        // jika REAR di akhir array, kembali ke awal
+        else if (REAR == max - 1)
+        {
+            REAR = 0;
+        }
+        else
+        {
+            REAR = REAR + 1;
+        }
+
+        queue_array[REAR] = num;
+    }
+
+    void remove()
+    {
+        // cek apakah queue kosong
+        if (FRONT == -1)
+        {
+            cout << "\nQueue underflow\n";
+            return;
+        }
+
+        cout << "\nThe element deleted from the queue is: "
+             << queue_array[FRONT] << "\n";
+
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            // jika elemen yang dihapus berada di posisi terakhir array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }
